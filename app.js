5e30be25a9e08263a2a36f76a259ef5df51fd37f
Josh <critesjosh@gmail.com>
2016-06-29 08:56:16 -0600

    Responsive design style changes
    Adjusted the CSS to show up better on mobile.

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ $('body').scrollspy({ target: '.navbar-fixed-top' });
 var message = $('#message').val();
 
  $("#submitMessage").on('click', function() {
-   $(this).button('complete')
+   conosle.log('hi');
  })
